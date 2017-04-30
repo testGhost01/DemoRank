6df4aadee966a04eae0440727909d59ed239cd01
Archana Tomar <archana.tomar88@gmail.com>
2017-04-30 23:42:47 +0530

    routing  bug fixed

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,12 +2,12 @@ import  { Routes }  from  '@angular/router';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
+import { SignUpPageComponent } from './sign-up-page/sign-up-page.component';
 
 export const  appRoutes  :  Routes =  [
 			{ path: '',  redirectTo: '/home',pathMatch: 'full'},
 			{ path: 'login', component: LoginPageComponent },
-		              { path: 'signUp', component: SignUpComponent },
+		              { path: 'signUp', component: SignUpPageComponent },
 		              {  path: 'home', component: HomePageComponent  },
 		              { path: '**', component: AppComponent  }
 ];
